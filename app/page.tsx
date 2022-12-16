a84a6a07726df44a6b9224f1e4e849d380a56893
Joosep Arrak <jooseparrak@gmail.com>
2022-12-16 20:55:05 +0200

    Use Next 13, add Sanity Studio to the app directly

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function HomePage() {
+  return (
+    <div>
+      <h1 className="text-4xl">Blog</h1>
+      <p>Some content</p>
+    </div>
+  );
+}
+
+export default HomePage;
