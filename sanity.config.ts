ad27b4da7817f7264d2af43b34dcc05dc9af4737
Joosep Arrak <jooseparrak@gmail.com>
2022-12-16 21:19:03 +0200

    Add custom navbar to Sanity Studio to redirect back to the homepage

--- sanity.config.ts
+++ sanity.config.ts
@@ -3,6 +3,7 @@ import { deskTool } from "sanity/desk";
 import { visionTool } from "@sanity/vision";
 import { schemaTypes } from "./schemas";
 import { myTheme } from "./theme";
+import StudioNavbar from "./components/StudioNavbar";
 
 const projectId: any = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
 const dataset: any = process.env.NEXT_PUBLIC_SANITY_DATASET;
@@ -16,6 +17,11 @@ export default defineConfig({
   plugins: [deskTool(), visionTool()],
   schema: {
     types: schemaTypes,
+  },
+  studio: {
+    components: {
+      navbar: StudioNavbar,
+    },
   },
   theme: myTheme,
 });
